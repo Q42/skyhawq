572a92a93da2c9a511515c281b4d52562f08f17d
Wouter van Drunen <wvandrunen@gmail.com>
2015-11-28 00:34:47 +0100

    Add upload server

---
+++ data-parser/server.js
@@ -0,0 +1,115 @@
+var http = require('http'),
+    path = require('path'),
+    os = require('os'),
+    fs = require('fs');
+
+var Busboy = require('busboy');
+var md5File = require('md5-file');
+var childProcess = require("child_process");
+
+var folder = "/Volumes/Crucial/upload";
+childProcess.execSync("mkdir -p /Volumes/Crucial/upload");
+
+function json(res, data) {
+	res.writeHead(200, {'Content-Type': 'application/json' });
+	res.end(JSON.stringify(data));
+}
+
+var methods = {
+	
+	index: function(req, res){
+		res.writeHead(200, {'Content-Type': 'text/html' });
+		res.end("Greanpeace data ingestion server<br><br>See <a href='http://localhost:8000/flights'>http://localhost:8000/flights</a>");
+		return;
+	},
+	
+	list: function(req, res) {
+		var folders = fs.readdirSync(folder);
+		res.writeHead(200, {'Content-Type': 'application/json' });
+		return json(res, folders.filter(noDotFiles).map(function(f){
+			var stat = fs.statSync(path.join(folder, f));
+			return {
+				id: f,
+				created: stat.ctime
+			}
+		}).sort(function(a, b) {
+			return b.created - a.created;
+		}));
+	},
+	
+	flight: function(req, res, flightId) {
+		if(!/^[a-z]+$/.test(flightId)) {
+			return json(res, {
+				"error": "Invalid flight name"
+			});
+		}
+		var files = fs.readdirSync(path.join(folder, flightId));
+		var fileStats = files.filter(noDotFiles).map(function(f){
+			var stat = fs.statSync(path.join(folder, flightId, f));
+			return {
+				id: f,
+				created: stat.ctime
+			}
+		});
+		json(res, {
+			lastModified: fileStats.sort(function(a,b){ return a.created -  b.created; }).last(),
+			files: fileStats
+		});
+		flightId
+		return;
+	},
+	
+	upload: function(req, res, flightId) {
+		var busboy = new Busboy({ headers: req.headers });
+		var files = [];
+		busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+			childProcess.execSync("mkdir -p "+path.join(folder, flightId));
+      var saveTo = path.join(folder, flightId, path.basename(fieldname));
+      file.pipe(fs.createWriteStream(saveTo));
+			file.on('end',function(){
+				files.push(md5File(saveTo));
+			});
+    });
+    busboy.on('finish', function() {
+      res.writeHead(200, { 'Connection': 'close' });
+      res.end(files.join(","));
+    });
+    return req.pipe(busboy);
+	}
+	
+}
+
+http.createServer(function(req, res) {
+	
+	// View
+	if (req.method === 'GET') {
+		if (req.url == "/flights") {
+			return methods.list(req, res);
+		}
+		if(req.url.indexOf("/flights/") == 0 && req.url.length > "/flights/".length) {
+			var id = req.url.substr("/flights/".length);
+			return methods.flight(req, res, id);
+		}
+		return methods.index(req, res);
+	}
+	
+	// Upload
+  if (req.method === 'POST' && req.url.indexOf("/upload/") == 0) {
+		var id = req.url.substr("/upload/".length);
+		return methods.upload(req, res, id);
+  }
+	
+	// 404
+  res.writeHead(404);
+  res.end();
+}).listen(8000, function() {
+  console.log('Listening for requests');
+});
+
+function noDotFiles(f){
+	return f[0] != '.';
+}
+
+Array.prototype.last = function(){
+	return this[this.length - 1];
+}
