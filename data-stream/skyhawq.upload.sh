7ec8e6b2b03e288ad8c9ec0ac809fea3f5ee68b9
Wouter van Drunen <wvandrunen@gmail.com>
2015-11-28 10:48:22 +0100

    Begin for auto-upload

--- data-stream/skyhawq.upload.sh
+++ data-stream/skyhawq.upload.sh
@@ -6,3 +6,16 @@ if [[ "$IFACE" -ne "eth0" ]]; then
 fi
 
 # Upload NOW
+
+exit 0;
+
+photos=`ls /var/skyhawq/*.jpg`
+md5=`md5sum $filename | head -n1 | sed -e 's/\s.*$//'`
+
+# Upload files this way:
+flightId=`date +"%y%m%d-%H%M%S"`
+curl \
+	-H "Secret: $SECRET" \
+  -H "Etag: $md5" \
+  -F "$filename=@$filename" \
+ "http://greenpeace.hermanbanken.nl/upload/$flightId"
