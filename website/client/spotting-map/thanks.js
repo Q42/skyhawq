7651538c4626400d4974be9bd3f8a3e1c3e7c27a
Wouter van Drunen <wvandrunen@gmail.com>
2015-11-28 13:33:47 +0100

    Fix a/an difference

--- website/client/spotting-map/thanks.js
+++ website/client/spotting-map/thanks.js
@@ -1,13 +1,16 @@
 Template.thanks.helpers({
-    'saved': function () {
-        var editedMarkers = Session.get('addedMarkers');
-        var counts = ["no", "a", "two", "three", "four", "five"];
+  'saved': function () {
+    var editedMarkers = Session.get('addedMarkers');
+    var counts = ["no", "a", "two", "three", "four", "five"];
 
-        var lines = _.chain(editedMarkers).countBy('description').pairs().value().map(function (item) {
-            return counts[item[1]] + " " + (item[1] == 1 ? item[0] : item[0] + "s");
-        });
-        var last = lines.pop();
-        var line = lines.length > 0 ? lines.join(", ") + " and " + last : last;
-        return line;
-    }
+    var lines = _.chain(editedMarkers).countBy('description').pairs().value().map(function (item) {
+      var prefix = counts[item[1]];
+						if (item[1] == 1 && /^(a|o|u|i|e)/.test(item[0].toLowerCase()))
+        prefix = "an";
+						return prefix + " " + (item[1] == 1 ? item[0] : item[0].trim() + "s");
+    });
+    var last = lines.pop();
+    var line = lines.length > 0 ? lines.join(", ") + " and " + last : last;
+    return line;
+  }
 });
