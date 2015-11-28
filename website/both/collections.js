f41156e17f4ae26ade5acaa0f45f25cb2898bad7
Johan Arensman <johan@fabrique.nl>
2015-11-28 14:38:09 +0100

    Added a skip when importing photo's

--- website/both/collections.js
+++ website/both/collections.js
@@ -1,24 +1,27 @@
 Images = new Mongo.Collection('images');
 
 var basePath = 'http://greenpeace.hermanbanken.nl/flights/',
-    flightId = '1';
+    flightId = '1',
+    skip = 500;
 
 if (Meteor.isServer && Images.find({flightId: flightId}).count() === 0) {
     HTTP.get(basePath + flightId + '/' + 'path', {}, function (error, result) {
         var data = result.data;
 
         // import photos
-        data.photos.forEach(function (photo) {
-            Images.insert({
-                'flightId': flightId,
-                'source': basePath + flightId + '/' + photo.photoPath,
-                'date': photo.date,
-                'coords': {
-                    'lat': photo.gps.latitude,
-                    'lng': photo.gps.longitude
-                },
-                'markers': []
-            });
+        data.photos.forEach(function (photo, index) {
+            if (index > skip) {
+                Images.insert({
+                    'flightId': flightId,
+                    'source': basePath + flightId + '/' + photo.photoPath,
+                    'date': photo.date,
+                    'coords': {
+                        'lat': photo.gps.latitude,
+                        'lng': photo.gps.longitude
+                    },
+                    'markers': []
+                });
+            }
         });
     });
 }
